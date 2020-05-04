56a9d12eb38337dc3c7714822e17aaa801a3e3c6
Julius Bächle <julius.baechle@yahoo.de>
2020-05-04 14:25:01 +0200

    IBarcodeScanner auf UInt64 erweitert

--- SmartFridge/BarcodeScanner/IBarcodeScanner.cs
+++ SmartFridge/BarcodeScanner/IBarcodeScanner.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartFridge.BarcodeScanner {
-    public delegate void BarcodeHandler(ulong barcode);
+    public delegate void BarcodeHandler(UInt64 barcode);
 
     public interface IBarcodeScanner {
         event BarcodeHandler ScannedBarcode;
