717f69c447b842dae1cef726f5016539aa7f6c95
Julius Bächle <julius.baechle@yahoo.de>
2020-05-04 19:13:06 +0200

    Modul Products angefangen

---
+++ SmartFridge/Products/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridge.Products
+{
+    enum EProductCategory
+    {
+        Other,
+        Fruit,
+        Vegetable,
+        Meat_Fish,
+        Dairy_Product,
+        Breakfast
+    }
+
+    class Product
+    {
+        public Product()
+        {
+            ID = Guid.NewGuid();
+            Barcodes = new List<UInt64>();            
+        }
+
+
+        public string Name {
+            get;
+            internal set;
+        }
+
+        public UInt16 Energy {
+            get;
+            internal set;
+        }
+
+        public UInt16 Durability {
+            get;
+            internal set;
+        }
+
+        public EProductCategory Category {
+            get;
+            internal set;
+        }
+
+        internal Guid ID {
+            get;
+            set;
+        }
+
+        internal List<UInt64> Barcodes {
+            get;
+            set;
+        }
+    }
+}
