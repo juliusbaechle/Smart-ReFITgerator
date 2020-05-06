3784cb13f7332728ae2460bd2d541814276a540a
Julius Bächle <julius.baechle@yahoo.de>
2020-05-06 10:07:25 +0200

    DBProducts gegen Interface DbCommand / DbConnection implementiert

--- SmartFridge/Products/DBProducts.cs
+++ SmartFridge/Products/DBProducts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SQLite;
 using System.Linq;
 using System.Security.Cryptography;
@@ -11,7 +12,7 @@ namespace SmartFridge.Products
 {
     class DBProducts
     {
-        public DBProducts(SQLiteConnection db)
+        public DBProducts(DbConnection db)
         {
             m_db = db;
             CreateTable();
@@ -19,7 +20,7 @@ namespace SmartFridge.Products
 
         public void Save(Product product)
         {
-            SQLiteCommand cmd = new SQLiteCommand(m_db);
+            DbCommand cmd = m_db.CreateCommand();
 
             if (!Contains(product.ID))
             {                
@@ -38,9 +39,9 @@ namespace SmartFridge.Products
 
         public List<Product> LoadAll()
         {
-            SQLiteCommand cmd = new SQLiteCommand(m_db);
+            DbCommand cmd = m_db.CreateCommand();
             cmd.CommandText = "SELECT * FROM tblProducts";
-            SQLiteDataReader reader = cmd.ExecuteReader();
+            DbDataReader reader = cmd.ExecuteReader();
 
             List<Product> products = new List<Product>();
             while (reader.Read())
@@ -58,14 +59,14 @@ namespace SmartFridge.Products
 
         public void Clear()
         {
-            SQLiteCommand cmd = new SQLiteCommand(m_db);
+            DbCommand cmd = m_db.CreateCommand();
             cmd.CommandText = "DELETE FROM tblProducts";
             cmd.ExecuteNonQuery();
         }
 
         private bool Contains(Guid productId)
         {
-            SQLiteCommand cmd = new SQLiteCommand(m_db);
+            DbCommand cmd = m_db.CreateCommand();
             cmd.CommandText = $"SELECT * FROM tblProducts WHERE Id = '{productId}'";
             bool contained = cmd.ExecuteScalar() != null;
             return contained;
@@ -73,11 +74,11 @@ namespace SmartFridge.Products
 
         private void CreateTable()
         {
-            SQLiteCommand cmd = new SQLiteCommand(m_db);
+            DbCommand cmd = m_db.CreateCommand();
             cmd.CommandText = "CREATE TABLE IF NOT EXISTS tblProducts (Id TEXT PRIMARY KEY, Name TEXT, Durability INT, Energy INT, Category INT )";
             cmd.ExecuteNonQuery();
         }
 
-        private SQLiteConnection m_db;
+        private DbConnection m_db;
     }
 }
--- SmartFridge/Program.cs
+++ SmartFridge/Program.cs
@@ -13,7 +13,8 @@ namespace SmartFridge {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            DB.Setup();
         }
     }
 }
