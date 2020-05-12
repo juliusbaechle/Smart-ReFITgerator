bac68acfd7a91ec1cc0413479926751d47e28391
Julius Bächle <julius.baechle@yahoo.de>
2020-05-12 13:34:33 +0200

    App wird jetzt in DependencyRoot zusammengeführt

---
+++ SmartFridge/DependencyRoot.cs
@@ -0,0 +1,42 @@
+﻿using SmartFridge.ProductNS;
+using SmartFridgeWPF;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartFridge
+{
+    // Hier werden alle Objekte, Datenverbindungen, ... erzeugt
+    // und im Rahmen der DependencyInjection übergeben
+
+    class DependencyRoot
+    {   
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            DependencyRoot.Setup();
+        }
+
+        private static void Setup()
+        {
+            Application app = Application.LoadComponent(new Uri("App.xaml", UriKind.Relative)) as Application;
+            var mainWindow = new MainWindow(app.Resources);
+
+            var db = DB.CreateLocalConnection();
+            var products = new Products(new DBProducts(db));
+            // var contents = new Contents(new DBContents(db));
+            // ...
+            
+            var mediator = new Mediator(mainWindow, products);
+            mediator.ShowPage(EPage.Home);    
+
+            app.Run(mainWindow);
+        }
+    }
+}
