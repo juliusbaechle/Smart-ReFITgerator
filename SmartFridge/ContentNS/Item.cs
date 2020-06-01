17ed66d43a027e466d5da480acc380bf39b91588
Julius Bächle <julius.baechle@yahoo.de>
2020-06-01 14:32:56 +0200

    Property Durability und Energy hinzugefügt

--- SmartFridge/ContentNS/Item.cs
+++ SmartFridge/ContentNS/Item.cs
@@ -14,10 +14,24 @@ namespace SmartFridge.ContentNS
 
         internal string ProductID { get; set; }
 
-        public DateTime m  { get; internal set; }
+        public DateTime ExpiryDate  { get; set; }
 
-        public UInt32 Amount { get; internal set; }
-        public Product Product { get; internal set; }
+        public UInt32 Amount { get; set; }
+        public Product Product { get; set; }
+
+        public int Durability { 
+            get { return (ExpiryDate - DateTime.Now).Days; } 
+        }
+
+        public uint Energy { 
+            get {
+                if (Product.Quantity == EQuantity.Count)
+                    return Amount * Product.Energy;
+                else
+                    return Amount * Product.Energy / 100; 
+            } 
+        }
+
         internal bool IsValid()
         {
             if (Amount <= 0) return false;
