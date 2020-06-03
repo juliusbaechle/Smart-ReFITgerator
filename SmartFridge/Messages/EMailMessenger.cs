84b79428a549c67daf7022068ca059f70bca0e0c
Julius Bächle <julius.baechle@yahoo.de>
2020-06-03 21:18:43 +0200

    EMailMessenger implementiert

---
+++ SmartFridge/Messages/EMailMessenger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SmartFridge.Messages
+{
+    class EMailMessenger : IMessenger
+    {
+        public EMailMessenger(string emailAddress)
+        {
+            ConnectionData = emailAddress;
+        }
+
+        public bool Send(Message msg)
+        {            
+            string from = "smart.refitgerator@gmail.com";
+            string to = ConnectionData;
+            var message = new MailMessage(from, to);
+            message.Subject = msg.Title;
+            message.Body = msg.Text;
+
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new NetworkCredential("smart.refitgerator@gmail.com", "refitgerator");
+            client.EnableSsl = true;
+
+            try
+            {
+                client.Send(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string ConnectionData { get; set; }
+    }
+}
