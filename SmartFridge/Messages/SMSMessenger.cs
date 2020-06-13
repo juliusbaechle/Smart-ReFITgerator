ed49cce4249cd9db42fc92dc6fd15abece25e0d7
Julius Bächle <julius.baechle@yahoo.de>
2020-06-13 09:27:16 +0200

    SMSMessenger angefangen (nicht funktionsfähig)

---
+++ SmartFridge/Messages/SMSMessenger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Nexmo.Api;
+using Nexmo.Api.Request;
+
+/*
+namespace SmartFridge.Messages
+{
+    class SMSMessenger : IMessenger
+    {
+        public SMSMessenger(string connectionData)
+        {
+            ConnectionData = connectionData;
+        }        
+
+        public bool Send(Message msg)
+        {
+            var creds = new Credentials { 
+                ApiKey = "9f1ab4a7", 
+                ApiSecret = "eYFtJwfSRE76zgd7" 
+            };
+
+            var sms = new SMS.SMSRequest {
+                from = "SmartFIT",
+                to = "4915902600345",
+                text = msg.Text
+            };
+            
+            try
+            {
+                SMS.Send(sms);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        public string ConnectionData { get; set; }
+    }
+}
+*/
