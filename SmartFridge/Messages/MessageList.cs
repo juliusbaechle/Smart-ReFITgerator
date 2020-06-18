8360573f898c6190d036a8b3df4b175d5afabcdb
Julius Bächle <julius.baechle@yahoo.de>
2020-06-18 14:53:03 +0200

    MessageList hinzugefügt

---
+++ SmartFridge/Messages/MessageList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SmartFridge.Messages
+{
+    public class MessageList
+    {
+        public void Add(IMessage msg)
+        {
+            if (Contains(msg)) return;
+            Msgs.Add(msg);
+        }
+
+        public void Remove(IMessage a_msg)
+        {
+            var copy = new List<IMessage>(Msgs);
+
+            foreach (IMessage msg in copy)
+                if (msg.Equals(a_msg)) 
+                    Msgs.Remove(msg);
+        }
+
+        public bool Contains(IMessage a_msg)
+        {
+            foreach(IMessage msg in Msgs)
+                if (msg.Equals(a_msg)) 
+                    return true;
+
+            return false;
+        }
+
+        public BindingList<IMessage> Msgs;
+    }
+}
