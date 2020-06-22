4a05ffb98fbd6a5d385981506c30f7bc99b73eeb
Julius Bächle <julius.baechle@yahoo.de>
2020-06-22 16:44:22 +0200

    ImagePage hinzugefügt

---
+++ SmartFridge/ImagePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace SmartFridge
+{
+    /// <summary>
+    /// Interaktionslogik für ImagePage.xaml
+    /// </summary>
+    public partial class ImagePage : Page
+    {
+        public ImagePage(string imagePath)
+        {
+            InitializeComponent();
+
+            #if DEBUG
+                imagePath = "../../pageImages/" + imagePath;
+            #else
+                imagePath = "pageImages/" + imagePath;
+            #endif
+
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.CacheOption = BitmapCacheOption.OnLoad;
+            img.UriSource = new Uri(imagePath, UriKind.Relative);
+            img.EndInit();
+            img.Freeze();
+            BackgroundImage.Source = img;
+        }
+    }
+}
