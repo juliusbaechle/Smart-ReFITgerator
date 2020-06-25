aeaaf11d41a14ae9b57fbc41782daecc3d12660b
Julius Bächle <julius.baechle@yahoo.de>
2020-06-25 19:32:11 +0200

    MainWindow zeigt Verbindungsstatus an

--- SmartFridge/MainWindow.xaml.cs
+++ SmartFridge/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using SmartFridge;
-using SmartFridge.ProductNS;
-using SmartFridgeWPF.ProductNS;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartFridgeWPF
 {
@@ -32,19 +18,31 @@ namespace SmartFridgeWPF
 
         private void ConnectButtons()
         {
-            btnHome.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Home); };
-            btnProducts.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Products); };
-            btnContent.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Content); };
-            btnNutrition.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Nutrition); };
-            btnMessages.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Messages); };
-            btnShopping.Click += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Shopping); };
-            btnClose.Click += (object sender, RoutedEventArgs e) => { Close(); };
+            btnHome.Click       += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Home); };
+            btnProducts.Click   += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Products); };
+            btnContent.Click    += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Content); };
+            btnNutrition.Click  += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Nutrition); };
+            btnMessages.Click   += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Messages); };
+            btnShopping.Click   += (object sender, RoutedEventArgs e) => { OpenPage?.Invoke(EPage.Shopping); };
+            btnClose.Click      += (object sender, RoutedEventArgs e) => { Close(); };
         }
 
         public void SetContent(Page page)
         {
             ContentFrame.NavigationService.RemoveBackEntry();
             ContentFrame.Content = page;
+        }
+
+        public void SetConnectionState(bool connected)
+        {
+            if (connected)
+            {
+                txtConnection.Text = "Verbunden";
+            }
+            else
+            {
+                txtConnection.Text = "Offline";
+            }
         }
     }
 }
