4929a8bd1a60ceb016828053e9596e0a265bf731
Julius Bächle <julius.baechle@yahoo.de>
2020-06-25 19:34:00 +0200

    DB: Unnötige Includes entfernt

--- SmartFridge/DB.cs
+++ SmartFridge/DB.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SQLite;
-using System.IO;
+﻿using System.Data.SQLite;
 using System.Data.Common;
 using MySql.Data.MySqlClient;
-using SmartFridge.ProductNS;
-using System.Windows;
 
 namespace SmartFridge
 {
-    internal class DB
+    public class DB
     {
         #if DEBUG
             // Relativ zur Debug.exe
