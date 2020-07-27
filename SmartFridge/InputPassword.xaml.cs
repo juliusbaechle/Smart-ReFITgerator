65b721fffa23e491e423d66b719b8e1f5cfbb362
Julius Bächle <julius.baechle@yahoo.de>
2020-07-27 12:08:29 +0200

    PasswordInput hinzugefügt

---
+++ SmartFridge/InputPassword.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace SmartFridge
+{
+    /// <summary>
+    /// Interaktionslogik für InputPassword.xaml
+    /// </summary>
+    public partial class InputPassword : Window
+    {
+        public InputPassword()
+        {
+            InitializeComponent();
+        }
+
+        public string Password {
+            get { return txtPassword.Password; } 
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
